d0d16c87dd77b4ffb30b90d8df0b834c4cd85bc9
Lucas Lima <lucass.limaa@outlook.com.br>
2023-12-06 21:53:03 -0300

    adjust code

--- program.ino
+++ program.ino
@@ -10,8 +10,8 @@ AF_DCMotor engine_b(2);
 
 char Direction;
 
-int state_rec;
-int vSpeed = 400; // Velocidade Default dos motores
+char state_rec;
+int vSpeed = 200; // Velocidade Default dos motores
 
 void setup()
 {
@@ -23,43 +23,48 @@ void loop()
   if(Serial.available() > 0){
     state_rec = Serial.read();
     Direction = state_rec;
-    Serial.println(state_rec);
-    Serial.println(state_rec == 1);
-  }
-  
-  if(Direction == 1 || Direction == 51)
-  {
-    stopEngine_A();
-    stopEngine_B();
-    startEngine_A_ToFront();
-  	startEngine_B_ToFront();
-  }
-  if(Direction == 2 || Direction == 52)
-  {
-    stopEngine_A();
-    stopEngine_B();
-    startEngine_A_ToBack();
-    startEngine_B_ToBack();
-  }
-  if(Direction == 3 || Direction == 53)
-  {
-    stopEngine_A();
-    stopEngine_B();
-    startEngine_A_ToBack();
-    startEngine_B_ToFront();
-  }
-  if(Direction == 4 || Direction == 54)
-  {
-    stopEngine_A();
-    stopEngine_B();
-    startEngine_A_ToFront();
-    startEngine_B_ToBack();
-  }
 
-  if(Direction == 'S')
-  {
-    stopEngine_A();
-    stopEngine_B();
+    if(Direction == 'F' || Direction == 'g')
+    {
+      startEngine_A_ToFront();
+      startEngine_B_ToFront();
+      delay(300);
+      stopEngine_A();
+      stopEngine_B();
+      Direction = 'A';
+    }
+
+    if(Direction == 'B')
+    {
+      stopEngine_A();
+      stopEngine_B();
+      Direction = 'A';
+    }
+
+    if(Direction == 'L')
+    {
+      startEngine_A_ToFront();
+      startEngine_B_ToBack();
+      delay(200);
+      stopEngine_A();
+      stopEngine_B();
+      Direction = 'A';
+    }
+
+    if(Direction == 'R')
+    {
+      startEngine_A_ToBack();
+      startEngine_B_ToFront();
+      delay(200);
+      stopEngine_A();
+      stopEngine_B();
+      Direction = 'A';
+    }
+
+    if(Direction == 'A' )
+    {
+      Serial.println("parada");
+    }
   }
 }
 
@@ -68,7 +73,6 @@ void loop()
 void stopEngine_A(){
   engine_a.setSpeed(0);
   engine_a.run(RELEASE);
-  delay(200);
 }
 
 void startEngine_A_ToFront(){
@@ -88,7 +92,6 @@ void startEngine_A_ToBack(){
 void stopEngine_B(){
   engine_b.setSpeed(0);
   engine_b.run(RELEASE);
-  delay(200);
 }
 
 void startEngine_B_ToFront(){
